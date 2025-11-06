6bcfa2a83ce5fd7063da7e508fa85e59a1663e31
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-11-06 01:38:18 -0400

    style(component): change blog styles

--- views/blogBySlug/components/Article/Article.tsx
+++ views/blogBySlug/components/Article/Article.tsx
@@ -23,66 +23,62 @@ export const Article: React.FC<Omit<BlogPost, 'meta'>> = ({
 
   return (
     <article className={styles[`article-box`]}>
-      <AppContainer
-        className={styles['article__intro']}
-        elementType='header'
-        bgColor={BG_STYLES_CLASSES.DARK}
-      >
-        <div className={styles[`article__intro--creative`]}>
-          <address className={styles[``]}>
-            <a
-              href={author.webAddress}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {author.name}
-            </a>
-          </address>{' '}
-          <span>•</span>{' '}
-          <time className={styles[``]} dateTime={date}>
-            {date}
-          </time>
+      <div className={styles['section_container']}>
+        <header className={styles['article__intro']}>
+          <div className={styles[`article__intro--creative`]}>
+            <address className={styles[``]}>
+              <a
+                href={author.webAddress}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                {author.name}
+              </a>
+            </address>{' '}
+            <span>•</span>{' '}
+            <time className={styles[``]} dateTime={date}>
+              {date}
+            </time>
+          </div>
+          <h1 className={styles[`article__intro--title`]}>{title}</h1>
+          <p className={styles[`article__intro--desc`]}>{shortDescription}</p>
+          <div className={styles[`article__intro--tags`]}>{tagChips}</div>
+          <div className={styles[`article__intro--img`]}>
+            <picture className={styles[``]}>
+              <source
+                srcSet={img.src.lg}
+                media='(min-width: 1020px)'
+                width='920'
+                height='520'
+              />
+              <source
+                srcSet={img.src.md}
+                media='(min-width: 720px)'
+                width='690'
+                height='370'
+              />
+              <source
+                srcSet={img.src.sm}
+                media='(min-width:520px)'
+                width='490'
+                height='270'
+              />
+              <img
+                loading={'eager'}
+                src={img.src.xs}
+                alt={img.alt}
+                width='320'
+                height='180'
+              />
+            </picture>
+          </div>
+        </header>
+      </div>
+      <div className={`${styles['section_container']} ${styles['bg-light']}`}>
+        <div className={styles['article__intro']}>
+          <RenderMD MD={blogMd} />
         </div>
-        <h1 className={styles[`article__intro--title`]}>{title}</h1>
-        <p className={styles[`article__intro--desc`]}>{shortDescription}</p>
-        <div className={styles[`article__intro--tags`]}>{tagChips}</div>
-        <div className={styles[`article__intro--img`]}>
-          <picture className={styles[``]}>
-            <source
-              srcSet={img.src.lg}
-              media='(min-width: 1020px)'
-              width='920'
-              height='520'
-            />
-            <source
-              srcSet={img.src.md}
-              media='(min-width: 720px)'
-              width='690'
-              height='370'
-            />
-            <source
-              srcSet={img.src.sm}
-              media='(min-width:520px)'
-              width='490'
-              height='270'
-            />
-            <img
-              loading={'eager'}
-              src={img.src.xs}
-              alt={img.alt}
-              width='320'
-              height='180'
-            />
-          </picture>
-        </div>
-      </AppContainer>
-      <AppContainer
-        className={styles['article__content']}
-        bgColor={BG_STYLES_CLASSES.LIGHT}
-        elementType='div'
-      >
-        <RenderMD MD={blogMd} />
-      </AppContainer>
+      </div>
       <AppContainer
         className={styles['article__footer']}
         bgColor={BG_STYLES_CLASSES.LIGHT}
